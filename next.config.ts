3c5d60c49428ffbd9a87c69a860f51924935d5b9
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-27 17:02:36 +0100

    add prettier

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import withRspack from 'next-rspack';
+import withRspack from "next-rspack";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	reactCompiler: true,
 };
 
 export default withRspack(nextConfig);
