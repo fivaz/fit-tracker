3f1e169ccca7ee3503af441d6b5b4b957338b8c8
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-27 16:51:19 +0100

    add rspack

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import withRspack from 'next-rspack';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,4 +6,4 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 };
 
-export default nextConfig;
+export default withRspack(nextConfig);
