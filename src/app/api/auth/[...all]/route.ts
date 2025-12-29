b8631625fb6271ddce44971bffb7b22534db8301
fivaz <31249205+fivaz@users.noreply.github.com>
2025-12-29 21:01:09 +0100

    add google and github oauth

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,13 +2,4 @@ import { toNextJsHandler } from "better-auth/next-js";
 
 import { auth } from "@/lib/auth"; // path to your auth file
 
-const handler = toNextJsHandler(auth);
-
-export const POST = async (req: Request) => {
-	console.log("--- [API ROUTE] POST request to:", req.url);
-	const res = await handler.POST(req);
-	console.log("--- [API ROUTE] Response Status:", res.status);
-	return res;
-};
-
-export const GET = handler.GET;
+export const { POST, GET } = toNextJsHandler(auth);
